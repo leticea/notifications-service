bf2b6aa0c5fbbe22f5cf46551c6ee84a3c5bb236
leticea <nascimento.leticeah@gmail.com>
2022-12-14 12:17:13 -0300

    criando in memory notifications repository

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,14 +1,6 @@
 import { Notification } from "../entities/notification";
 import { SendNotification } from "./send-notification";
 
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
-
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
     const sendNotification = new SendNotification(notificationsRepository);
@@ -18,8 +10,6 @@ describe('Send notification', () => {
       category: 'social',
       recipientId: 'example-recipient-id',
     });
-
-    console.log(notifications);
 
     expect(notifications).toHaveLength(1);
   });
