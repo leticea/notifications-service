f34dc26caf4c857847a0397bd17e7fa0a5ffd2e2
leticea <nascimento.leticeah@gmail.com>
2022-12-14 09:49:15 -0300

    criando entidade de notificação

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,25 +1,51 @@
 export interface NotificationProps {
+  recipientId: string;
   content: string;
   category: string;
+  readAt?: Date | null;
+  createdAt: Date;
 }
 
 export class Notification {
   private props: NotificationProps;
 
+  constructor(props: NotificationProps) {    
+    this.props = props;
+  }
 
-  constructor() {
-    this.content = '';
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
   }
 
   public set content(content: string) {
-    if (content.length < 5) {
-      throw new Error();
-    }
-
     this.props.content = content;
   }
+
+  public get content(): string {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
 }
-
-const notification = new Notification();
-
-notification.content = 'dadadad';
