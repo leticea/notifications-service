cfb7356e94afc3db690185b3f35e868ce8a175d4
leticea <nascimento.leticeah@gmail.com>
2022-12-15 16:12:13 -0300

    criando notification factory

--- test/factories/notification-factory.ts
+++ test/factories/notification-factory.ts
@@ -1,3 +1,10 @@
+import { Content } from "@application/entities/content";
+import { Notification } from "@application/entities/notification";
+
 export function makeNotification() {
-  
+  return new Notification({
+    category: 'social',
+    content: new Content('Nova solicitação de amizade!'),
+    recipientId: 'recipient-2',
+  });
 }
