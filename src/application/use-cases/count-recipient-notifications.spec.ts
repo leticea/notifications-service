7f7a88d93537a3c5358913f171053515b88d361b
leticea <nascimento.leticeah@gmail.com>
2022-12-15 16:00:01 -0300

    criando count recipient notifications

---
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -0,0 +1,29 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CountRecipientNotifications } from './count-recipient-notifications';
+
+describe('Count recipients notifications', () => {
+  it('should be able to count recipient notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countRecipientNotifications = new CountRecipientNotifications(
+      notificationsRepository,
+    );
+
+    const notification = new Notification({
+      category: 'social',
+      content: new Content('Nova solicitação de amizade!'),
+      recipientId: 'example-recipient-id',
+    });
+
+    await notificationsRepository.create(notification);
+
+    await countRecipientNotifications.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+});
