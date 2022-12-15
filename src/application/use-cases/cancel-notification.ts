32c5aabfde0f8f9c17f4d4c696bdf78a0a9e972f
leticea <nascimento.leticeah@gmail.com>
2022-12-15 13:10:56 -0300

    criando cancelNotification

---
+++ src/application/use-cases/cancel-notification.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface CancelNotificationRequest {
+  notificationId: string;
+}
+
+type CancelNotificationResponse = void;
+
+@Injectable()
+export class CancelNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: CancelNotificationRequest
+  ): Promise<CancelNotificationResponse> {
+    const { notificationId } = request;
+
+  }
+}
